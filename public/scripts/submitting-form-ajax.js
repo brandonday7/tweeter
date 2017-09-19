3736859fd602803b4065ffa4af3fc07580ba80d5
Brandon Day <brandontday7@gmail.com>
2017-09-19 23:34:08 +0000

    get request for tweets thru ajax, tweets render. submission must be 1-140 chars to POST

--- public/scripts/submitting-form-ajax.js
+++ public/scripts/submitting-form-ajax.js
@@ -1,17 +1,37 @@
 $(document).ready(function () {
   $('form').on('submit', function (event) {
     event.preventDefault();
-    let inputText = $(this).serialize();
 
+    let newTweetElements = $(this).parent().children();
+    if (newTweetElements.length > 2) {
+      newTweetElements.last().remove();
+    }
+
+
+    let currentTweet = $('textarea[name=text]').val();
+    if (currentTweet === '' || currentTweet === null) {
+      let errorMessage = "You cannot submit an empty field";
+      let errorChild = $('<h5>').text(errorMessage).addClass('error-message');
+      $(this).parent().append(errorChild);
+      return;
+
+    } else if (currentTweet.length > 140) {
+      let errorMessage = "Tweet content too long!";
+      let errorChild = $('<h5>').text(errorMessage).addClass('error-message');
+      $(this).parent().append(errorChild);
+      return;
+    }
+
+    let inputText = $(this).serialize();
 
     $.ajax({
       url: '/tweets/',
       method: 'POST',
       data: inputText,
       success: function () {
-        console.log('ajax succeeded', data);
+        console.log('ajax succeeded');
       }
     });
-
+    return;
   })
 })
